2351055c6e8a2720b14c7726ae823b0aa833e9c7
Michel Grolet <michelgrolet@gmail.com>
2022-07-27 10:21:50 +0200

    Update create_table.sql

--- resources/sql/create_table.sql
+++ resources/sql/create_table.sql
@@ -16,6 +16,20 @@ CREATE TABLE compte (
 -- Plus important, on va laisser cet insert d'admin comme premier insert de compte, toujours
 INSERT INTO compte (nom, prenom, adr_mail, mdp, privilege, url_p) VALUES ('Org', 'Litra', 'litra@gmail.com', '$2y$10$TYumsGQvtgC11kSj9oPPquYRj7T92k2JeQKb490ULoLsae0Xyep2q', 2, "1.png");
 
+-- Tous les mots de passe, sauf celui de l'admin, sont "mdp"
+
+-- Vendeurs
+INSERT INTO compte (nom, prenom, adr_mail, mdp, privilege) VALUES ('Matthews', 'Kian', 'mk@gmail.com', '$2y$10$PeEzU8lwWyifV4MDjYn5HeSJzeb08IXyvhE72.x7Dxlt3zcMW2.6W', 1);
+
+-- Organisateurs
+INSERT INTO compte (nom, prenom, adr_mail, mdp, num_tel, privilege, url_p) VALUES ('Chauvet', 'Lucille', 'org@gmail.com', '$2y$10$PeEzU8lwWyifV4MDjYn5HeSJzeb08IXyvhE72.x7Dxlt3zcMW2.6W', 0695032336, 3, "4.jpg");
+INSERT INTO compte (nom, prenom, adr_mail, mdp, privilege) VALUES ('Austin', 'Neville', 'Austinevil@gmail.com', '$2y$10$PeEzU8lwWyifV4MDjYn5HeSJzeb08IXyvhE72.x7Dxlt3zcMW2.6W', 3);
+
+
+-- Clients
+INSERT INTO compte (nom, prenom, adr_mail, mdp, num_tel, url_p) VALUES ('Richards', 'Kim', 'kim@gmail.com', '$2y$10$PeEzU8lwWyifV4MDjYn5HeSJzeb08IXyvhE72.x7Dxlt3zcMW2.6W', 0179728109, "2.jpg");
+INSERT INTO compte (nom, prenom, adr_mail, mdp, num_tel, url_p) VALUES ('Campbell', 'Adam', 'adam@gmail.com', '$2y$10$PeEzU8lwWyifV4MDjYn5HeSJzeb08IXyvhE72.x7Dxlt3zcMW2.6W', 0695032336, "6.jpg");
+INSERT INTO compte (nom, prenom, adr_mail, mdp) VALUES ('Beast', 'Jules', 'Jules.B@gmail.com', '$2y$10$PeEzU8lwWyifV4MDjYn5HeSJzeb08IXyvhE72.x7Dxlt3zcMW2.6W');
 
 CREATE TABLE article(
     id_article INTEGER NOT NULL AUTO_INCREMENT,
@@ -43,6 +57,13 @@ CREATE TABLE monnaie (
     PRIMARY KEY (id_monnaie)
 );
 
+INSERT INTO monnaie (id_createur, nom_monnaie, valeur) VALUES (1, 'LitraToken', 1);
+INSERT INTO monnaie (id_createur, nom_monnaie, valeur) VALUES (1, 'Florin', 4.5);
+INSERT INTO monnaie (id_createur, nom_monnaie, valeur) VALUES (1, 'Orin', 1.5);
+INSERT INTO monnaie (id_createur, nom_monnaie, valeur) VALUES (1, 'Couronne', 1.5);
+INSERT INTO monnaie (id_createur, nom_monnaie, valeur) VALUES (4, 'Token du Val', 2.2);
+INSERT INTO monnaie (id_createur, nom_monnaie, valeur) VALUES (5, 'Bayord', 1.8);
+
 CREATE TABLE evenement (
     id_evenement INTEGER NOT NULL AUTO_INCREMENT,
     id_createur INTEGER NOT NULL,
@@ -57,6 +78,11 @@ CREATE TABLE evenement (
     PRIMARY KEY (id_evenement)
 );
 
+INSERT INTO evenement (id_createur, id_monnaie, nom, description, lieu, img) VALUES (1, 1, "Lancement de Litra", "C'est la fin du module", "QG", "1.jpg");
+INSERT INTO evenement (id_createur, id_monnaie, nom, description, lieu, img) VALUES (4, 5, "Marché du haut Val", "Au printemps", "Val de Brei", "2.jpg");
+INSERT INTO evenement (id_createur, id_monnaie, nom, description, lieu, img) VALUES (4, 5, "Fête du Val", "En hiver", "Val de Brei", "3.jpg");
+INSERT INTO evenement (id_createur, id_monnaie, nom, description, lieu, img) VALUES (5, 6, "Cabaret gris", "Musique et BD", "Charleville", "4.jpg");
+
 CREATE TABLE rvendeurevenement (
     id_vendeur INTEGER NOT NULL,
     id_evenement INTEGER NOT NULL,
@@ -64,6 +90,7 @@ CREATE TABLE rvendeurevenement (
     FOREIGN KEY (id_evenement) REFERENCES evenement(id_evenement),
     PRIMARY KEY (id_vendeur, id_evenement)
 );
+
 
 CREATE TABLE transactions (
     id_transac INTEGER NOT NULL AUTO_INCREMENT,
@@ -85,3 +112,5 @@ CREATE TABLE logrfid (
     id_vendeur text NOT NULL,
     PRIMARY KEY (id_log)
 );
+
+INSERT INTO logrfid (carte_rfid, id_vendeur) VALUES ('2600DDE2A8B1', 4);
